3a7db71c4857b0e88a2d82e4b2ae788728b29dd5
Aagam Doshi <aagamdoshi1100@gmail.com>
2022-10-01 18:08:23 +0530

    feat : added translation

---
+++ App.js
@@ -0,0 +1,32 @@
+var URL = "https://api.funtranslations.com/translate/ferb-latin.json";
+
+
+var Input1 = document.querySelector("#txt-area");
+var Btn = document.querySelector("#translate");
+var Output = document.querySelector("#Output");
+
+
+Btn.addEventListener("click",ConstructURL);
+var Ctext = Input1.value;
+console.log(Ctext);
+
+function ConstructURL(){
+
+    var Ctext = Input1.value;
+
+    var finalURL = URL + "?" + "text=" + Ctext;
+    console.log(finalURL);
+    dataConversion(finalURL);
+
+}
+
+function dataConversion(finalURL){
+
+    fetch(finalURL).then(response => response.json()).then(json => {console.log(json); 
+        
+        var translatedtext = json.contents.translated;
+        Output.innerText = translatedtext;
+    
+    })
+
+}
